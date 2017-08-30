85daf05928bf67ee6cc32d42995f6bbecf55399a
Yang Bo <pop.atry@gmail.com>
2017-08-30 22:18:14 +0800

    Add MultipleException

---
+++ MultipleException/src/main/scala/com/thoughtworks/zerocost/MultipleException.scala
@@ -0,0 +1,60 @@
+package com.thoughtworks.zerocost.MultipleException
+
+import java.io.{PrintStream, PrintWriter}
+
+import cats.Semigroup
+
+object MultipleException {
+  object throwableSemigroup extends Semigroup[Throwable] {
+    override def combine(f1: Throwable, f2: Throwable): Throwable =
+      f1 match {
+        case MultipleException(exceptionSet1) =>
+          f2 match {
+            case MultipleException(exceptionSet2) => MultipleException(exceptionSet1 ++ exceptionSet2)
+            case _: Throwable                     => MultipleException(exceptionSet1 + f2)
+          }
+        case _: Throwable =>
+          f2 match {
+            case MultipleException(exceptionSet2) => MultipleException(exceptionSet2 + f1)
+            case _: Throwable                     => MultipleException(Set(f1, f2))
+          }
+      }
+  }
+}
+
+final case class MultipleException(throwableSet: Set[Throwable]) extends Exception("Multiple exceptions found") {
+  override def toString: String = throwableSet.mkString("\n")
+
+  override def printStackTrace(): Unit = {
+    for (throwable <- throwableSet) {
+      throwable.printStackTrace()
+    }
+  }
+
+  override def printStackTrace(s: PrintStream): Unit = {
+    for (throwable <- throwableSet) {
+      throwable.printStackTrace(s)
+    }
+  }
+
+  override def printStackTrace(s: PrintWriter): Unit = {
+    for (throwable <- throwableSet) {
+      throwable.printStackTrace(s)
+    }
+  }
+
+  override def getStackTrace: Array[StackTraceElement] = synchronized {
+    super.getStackTrace match {
+      case null =>
+        setStackTrace(throwableSet.flatMap(_.getStackTrace)(collection.breakOut))
+        super.getStackTrace
+      case stackTrace =>
+        stackTrace
+    }
+  }
+
+  override def fillInStackTrace(): this.type = {
+    this
+  }
+
+}
--- build.sbt
+++ build.sbt
@@ -12,4 +12,6 @@ lazy val `resourcet-covariant` = project.dependsOn(`parallel-covariant`, LiftIO)
 
 lazy val raii = project.dependsOn(`resourcet-covariant`, task)
 
+lazy val MultipleException = project
+
 crossScalaVersions in ThisBuild := Seq("2.11.11", "2.12.3")
