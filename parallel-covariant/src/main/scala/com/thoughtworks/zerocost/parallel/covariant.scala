294cdfe899e7695eed55eccdfcc0a1667151d14d
Yang Bo <pop.atry@gmail.com>
2017-08-30 20:22:02 +0800

    Add liftTypeClass method

--- parallel-covariant/src/main/scala/com/thoughtworks/zerocost/parallel/covariant.scala
+++ parallel-covariant/src/main/scala/com/thoughtworks/zerocost/parallel/covariant.scala
@@ -12,13 +12,14 @@ object covariant {
 
     def toParallel[F[+ _], A](fa: F[A]): Parallel[F, A]
     def fromParallel[F[+ _], A](parallel: Parallel[F, A]): F[A]
-
+    def liftTypeClass[M[_[_]], F[+ _]](typeClass: M[F]): M[Parallel[F, `+?`]]
   }
 
   private[covariant] val opacityTypes: OpacityTypes = new OpacityTypes {
     type Parallel[F[+ _], +A] = F[A]
     def toParallel[F[+ _], A](fa: F[A]): Parallel[F, A] = fa
     def fromParallel[F[+ _], A](parallel: Parallel[F, A]): F[A] = parallel
+    def liftTypeClass[M[_[_]], F[+ _]](typeClass: M[F]): M[Parallel[F, `+?`]] = typeClass
   }
 
   type Parallel[F[+ _], +A] = opacityTypes.Parallel[F, A]
@@ -26,6 +27,8 @@ object covariant {
 
     def apply[F[+ _], A](fa: F[A]): Parallel[F, A] = opacityTypes.toParallel(fa)
     def unapply[F[+ _], A](parallel: Parallel[F, A]): Some[F[A]] = Some(opacityTypes.fromParallel[F, A](parallel))
+
+    def liftTypeClass[M[_[_]], F[+ _]](typeClass: M[F]): M[Parallel[F, `+?`]] = opacityTypes.liftTypeClass(typeClass)
 
   }
 
